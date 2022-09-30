3cd53f4982a82b557d9d7f96d7f485d16759e18a
Oleg Lutsik <114278831+CleanRound@users.noreply.github.com>
2022-09-30 18:17:05 +0300

    Update: Upload hm_ex.py files from 27.09.22

---
+++ Branching Operators/Branching Operator (27.09.22)/hm_ex_1(27.09.22).py
@@ -0,0 +1,18 @@
+day = int(input("Enter the number of the day of the week from 1-7:\n"))
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("Error, type the correct number of the week.")
---
+++ Branching Operators/Branching Operator (27.09.22)/hm_ex_3(27.09.22).py
@@ -0,0 +1,10 @@
+number = int(input("Enter the number:\n"))
+
+if number > 0:
+    print("Number is positive.")
+elif number < 0:
+    print("Number is negative.")
+elif number == 0:
+    print("Number is equal to zero.")
+else:
+    print("Error, something is wrong with your typed number.")
