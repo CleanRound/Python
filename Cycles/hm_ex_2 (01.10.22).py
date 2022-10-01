19fd7b525826a62679632dfa1a1df79542feb83c
Oleg Lutsik <114278831+CleanRound@users.noreply.github.com>
2022-10-01 11:48:47 +0300

    Update: upload hm_ex.py files from 01.10.22

---
+++ Cycles/hm_ex_2 (01.10.22).py
@@ -0,0 +1,16 @@
+number_1 = int(input("Enter the first number: "))
+number_2 = int(input("Enter the second number: "))
+
+for num in range(number_1, number_2 + 1):
+    print(num, end=' ')
+print('')
+for num in reversed(range(number_1, number_2 + 1)):
+    print(num, end=' ')
+print('')
+for num in range(number_1, number_2 + 1):
+    if num % 7 == 0:
+        print(num, end=' ')
+print('')
+for num in range(number_1, number_2 + 1):
+    if num % 5 == 0:
+        print(num, end=' ')
