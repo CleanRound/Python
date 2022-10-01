9460480783b25d684118406b316304be51c26653
Oleg Lutsik <114278831+CleanRound@users.noreply.github.com>
2022-10-01 11:38:39 +0300

    Delete hm_ex_1(01.10.22).py

--- Cycles/hm_ex_1(01.10.22).py
+++
@@ -1,6 +0,0 @@
-number_1 = int(input("Enter the first number: "))
-number_2 = int(input("Enter the second number: "))
-
-for num in range(number_1, number_2 + 1):
-    if num % 7 == 0:
-        print(num, end=' ')
